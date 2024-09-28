42f29b9d98e924aaf2ee9f7694960960b7747219
Saad Qureshi <79354254+msaadqureshi@users.noreply.github.com>
2024-09-28 15:43:39 +0400

    Update SocialIcon.tsx

--- src/components/socials/SocialIcon.tsx
+++ src/components/socials/SocialIcon.tsx
@@ -2,13 +2,14 @@ import { SocialLinkType } from '@/lib/types';
 
 import SocialLink from './SocialLink';
 import { Icon } from '@iconify/react';
+import Image from 'next/image';
 
 const SocialIcon = ({ icon, url }: SocialLinkType) => {
   return (
     <li>
       <SocialLink href={url}>
         {/* <Icon icon={icon} width={24} height={24} /> */}
-        <img src={icon} alt={icon} width="25" height="25" />
+        <Image src={icon} alt={icon} width="25" height="25" />
       </SocialLink>
     </li>
   );
